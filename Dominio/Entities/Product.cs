95e110103b8ef0d1008f55863ba814cf113790e1
AlbertRojas90 <albert.rojas097@gmail.com>
2025-09-06 10:17:46 -0500

    puerto secundadio IRepository

---
+++ Dominio/Entities/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entities
+{
+    public class Product
+    {
+        private string _name;
+        private decimal _price;
+        public Guid Id { get; set; }
+
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("El nombre no puede ir vacio");
+                }
+                _name = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("El precio debe ser mayor a cero");
+                }
+                _price = value;
+            }
+        }
+
+        public Product(Guid id, string name, decimal price) 
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+
+
+    }
+}
---
+++ Dominio/Ports/Secundary/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Dominio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Ports.Secundary
+{
+    public interface IRepository
+    {
+        void Save(Product product);
+        List<Product> GetAll();
+    }
+}
